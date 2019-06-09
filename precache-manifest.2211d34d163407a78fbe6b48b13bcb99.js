09bcf0dd9af0070dab00f5b0f05550bdfb61e09f
tahaamaidi92 <benhama.taha.33@gmail.com>
2019-06-10 00:17:00 +0100

    Updates

---
+++ precache-manifest.2211d34d163407a78fbe6b48b13bcb99.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ab589f4cdcfcf1038b228d2c35838e1",
+    "url": "/Weather-App/index.html"
+  },
+  {
+    "revision": "2b3ac0405e115430761e",
+    "url": "/Weather-App/static/css/main.f1343ed2.chunk.css"
+  },
+  {
+    "revision": "5a304319f8b2fee3c054",
+    "url": "/Weather-App/static/js/2.47ce9c47.chunk.js"
+  },
+  {
+    "revision": "2b3ac0405e115430761e",
+    "url": "/Weather-App/static/js/main.22eafdfe.chunk.js"
+  },
+  {
+    "revision": "89bb695666ed5c38f231",
+    "url": "/Weather-App/static/js/runtime~main.671df5a1.js"
+  }
+]);
